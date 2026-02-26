9db531eee4ddcc4a6001c9dc92c4099660e1fdfc
nrilu <11998876+nrilu@users.noreply.github.com>
2026-02-26 12:23:47 +0100

    allow dimacs reporting

--- src/krite.c
+++ src/krite.c
@@ -81,10 +81,10 @@ void kissat_report_dimacs (kissat *solver) {
   size_t imported = SIZE_STACK (solver->import);
   if (imported) imported--; //first variable on the stack is dummy, to have it start on index 1
 
-  if (GET_OPTION(mallob_is_shweeper) && solver->shweeper_terminated_externally) {
-    kissat_custom_message (solver, 1, "SWEEPER will not report dimacs because was already externally terminated");
-    return;
-  }
+  // if (GET_OPTION(mallob_is_shweeper) && solver->shweeper_terminated_externally) {
+    // kissat_custom_message (solver, 1, "SWEEPER will not report dimacs because was already externally terminated");
+    // return;
+  // }
   if (GET_OPTION(mallob_is_shweeper) && solver->inconsistent) {
     kissat_custom_message (solver, 1, "SWEEPER will not report dimacs because is inconsistent (UNSAT)");
     return;
