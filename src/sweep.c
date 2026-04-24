1e469a2c90eaae520a8664796f9a86e1cb7746cb
nrilu <11998876+nrilu@users.noreply.github.com>
2026-04-24 16:11:44 +0200

    clean up assert messages

--- src/sweep.c
+++ src/sweep.c
@@ -2451,8 +2451,8 @@ void shweep_import_single_unit(sweeper *sweeper, unsigned ilit) {
   }
   // kissat_custom_message(solver, V4_UVERB_SWEEP," importing idx(%i),lit(%i) as repr_lit(%i)", IDX(repr_ilit), ilit, repr_ilit);
 
-  assert (!solver->values[repr_ilit]       || kissat_custom_assert_message ("Sweep ERROR : assigning repr_ilit %u (original lit %i), but already has a value %i", repr_ilit, ilit, solver->values[repr_ilit]));
-  assert (!solver->values[NOT(repr_ilit)]  || kissat_custom_assert_message ("Sweep ERROR : assigning not_repr_ilit %u, but already has a value %i", NOT(repr_ilit), solver->values[NOT(repr_ilit)]));
+  assert (!solver->values[repr_ilit]       || kissat_custom_assert_message (solver, "Sweep ERROR : assigning repr_ilit %u (original lit %i), but already has a value %i", repr_ilit, ilit, solver->values[repr_ilit]));
+  assert (!solver->values[NOT(repr_ilit)]  || kissat_custom_assert_message (solver, "Sweep ERROR : assigning not_repr_ilit %u, but already has a value %i", NOT(repr_ilit), solver->values[NOT(repr_ilit)]));
   kissat_assign_unit (solver, repr_ilit, "shweep imported unit");
   solver->shweep.units_useful++;
   INC (sweep_units);
@@ -2563,8 +2563,12 @@ void shweep_import_SweepJob_units(sweeper *sweeper) {
       break;
 
     count++;
-    assert(VALID_EXTERNAL_LITERAL (elit) || kissat_custom_assert_message ("Sweeper ERROR : imported invalid external elit %i ", elit));
+    assert(VALID_EXTERNAL_LITERAL (elit) || kissat_custom_assert_message (solver, "Sweeper ERROR : imported invalid external elit %i ", elit));
     unsigned ilit = kissat_import_literal (solver, elit);
+    if (ilit==INVALID_LIT) {
+      kissat_custom_message (solver, V1_INFO_SWEEP, "import:  i(%u) <- e[%i] skipped - is already eliminated locally ",ilit,elit);
+      continue;
+    }
     kissat_custom_message (solver, V1_INFO_SWEEP, "import:  i(%u) <- e[%i]",ilit,elit);
     shweep_import_single_unit (sweeper, ilit);
   }
@@ -3346,7 +3350,7 @@ int mallob_shweep_single_iteration(kissat *solver) {
 
   assert (!solver->level);
   assert (!solver->unflushed);
-  assert( !solver->probing);
+  // assert( !solver->probing);
 
   START (sweep);
   INC (sweep);
